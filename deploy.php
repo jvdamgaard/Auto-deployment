fa3b012fa610cdda325e36393381e7116d39f6ff
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2012-09-15 19:59:47 +0200

    Refactor and test

--- deploy.php
+++ deploy.php
@@ -21,20 +21,25 @@ $settings =	array(
                  'username' => 'jvdamgaard',
                  'repository' => 'm.adaptivedesignstudio.com',
                  'branches' => array(
-                         'master' => dirname(__FILE__),
-                         'release-*' => dirname(dirname(__FILE__)).'/test-m.adaptivedesignstudio.com',
-                         'hotfix_*' => dirname(dirname(__FILE__)).'/test-m.adaptivedesignstudio.com',
-                         'develop' => dirname(dirname(__FILE__)).'/dev-m.adaptivedesignstudio.com'
+                         'master' => dirname(dirname(__FILE__)),
+                         'release-*' => dirname(dirname(dirname(__FILE__))).'/test-m.adaptivedesignstudio.com',
+                         'hotfix_*' => dirname(dirname(dirname(__FILE__))).'/test-m.adaptivedesignstudio.com',
+                         'develop' => dirname(dirname(dirname(__FILE__))).'/dev-m.adaptivedesignstudio.com'
                      )
              );
 
-include_once('deploy/githubautodeployment.php');
+include_once('githubautodeployment.php');
+
+// debug
+//$payload = '{\"pusher\":{\"name\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"},\"repository\":{\"name\":\"m.adaptivedesignstudio.com\",\"created_at\":\"2012-09-10T13:10:41-07:00\",\"size\":696,\"has_wiki\":true,\"private\":false,\"watchers\":0,\"language\":\"JavaScript\",\"url\":\"https://github.com/jvdamgaard/m.adaptivedesignstudio.com\",\"fork\":false,\"pushed_at\":\"2012-09-15T03:42:08-07:00\",\"has_downloads\":true,\"open_issues\":0,\"has_issues\":true,\"description\":\"Testsite for mobile inteaction\",\"stargazers\":0,\"forks\":0,\"owner\":{\"name\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"}},\"forced\":false,\"after\":\"2227ca497f2e71d6a8d35ed5b2aa438be2200a3f\",\"head_commit\":{\"modified\":[\"index.html\"],\"added\":[],\"removed\":[],\"timestamp\":\"2012-09-15T03:41:57-07:00\",\"author\":{\"name\":\"Jakob Viskum Damgaard\",\"username\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"},\"url\":\"https://github.com/jvdamgaard/m.adaptivedesignstudio.com/commit/2227ca497f2e71d6a8d35ed5b2aa438be2200a3f\",\"id\":\"2227ca497f2e71d6a8d35ed5b2aa438be2200a3f\",\"distinct\":true,\"message\":\"dgf\",\"committer\":{\"name\":\"Jakob Viskum Damgaard\",\"username\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"}},\"deleted\":false,\"ref\":\"refs/heads/master\",\"commits\":[{\"modified\":[\"index.html\"],\"added\":[],\"removed\":[],\"timestamp\":\"2012-09-15T03:41:57-07:00\",\"author\":{\"name\":\"Jakob Viskum Damgaard\",\"username\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"},\"url\":\"https://github.com/jvdamgaard/m.adaptivedesignstudio.com/commit/2227ca497f2e71d6a8d35ed5b2aa438be2200a3f\",\"id\":\"2227ca497f2e71d6a8d35ed5b2aa438be2200a3f\",\"distinct\":true,\"message\":\"dgf\",\"committer\":{\"name\":\"Jakob Viskum Damgaard\",\"username\":\"jvdamgaard\",\"email\":\"jakob_damgaard@hotmail.com\"}}],\"before\":\"8159fe4cf1fb827f8652c8b442559888a15519b1\",\"compare\":\"https://github.com/jvdamgaard/m.adaptivedesignstudio.com/compare/8159fe4cf1fb...2227ca497f2e\",\"created\":false}';
 
 // Auto deploy form GitHub
 if(isset($_POST['payload']) && !empty($_POST['payload'])) {
     $deploy =   new GitHubAutoDeployment(
 					array_merge(
 						array('payload' => $_POST['payload']),
+                        // debig
+                        //array('payload' => $payload),
 						$settings
 					)
 				);
--- githubautodeployment.php
+++ githubautodeployment.php
@@ -3,25 +3,34 @@
  *  Main class itself where all the magic happens
  */
 
-include_once('deploy/yuicompressor.php');
+include_once('yuicompressor.php');
 
 class GitHubAutoDeployment {
 
-    const   LOG_FILE        = './deploy/log.txt';       // where to save all deploy results
+    const   LOG_FILE        = './log.txt';       // where to save all deploy results
 
     public  $data           = false;                    // what we received from Github
     public  $branch         = false;					// Actual branch to deploy from
     public  $settings       = array(
                                 'payload' => null,
                                 'username' => null,		// GitHub username
-                                'repository' => null,			// repository on GitHub
+                                'repository' => null,	// repository on GitHub
                                 'branches' => null,		// array - key: branch (* could be used alone or in beginnng or end of string); val: absolute path to deploye branch in
                                 'compileLess' => true,
                                 'compressJs' => true,
                                 'compressCss' => true,
                                 'compressHtml' => true,
-                                'compressImages' => true
-                            )
+                                'compressImages' => true,
+                                'excludeFiles' => array(    // list of files you want to exclude from a deploy
+                                    '*.gitattributes',
+                                    '*.gitignore',
+                                    '*.md',
+                                    '*.less'
+                                ),
+                                'excludeDirs' => array(      // list of folders you want to exclude from a deploy
+                                    'less'
+                                )
+                            );
 
     public  $files          = array();                  // list of files to process on a server
     public  $ips            = array(                    // list of allowed IPs for a deploy. Defaults are Github IPs
@@ -29,53 +38,43 @@ class GitHubAutoDeployment {
                                 '50.57.128.197',
                                 '108.171.174.178'
                             );
-    public  $ex_files       = array(                    // list of files you want to exclude from a deploy
-                                '*.gitattributes',
-                                '*.gitignore',
-                                '*.md',
-                                '*.less'
-                            );
-    public  $ex_dirs        = array(                    // list of folders you want to exclude from a deploy
-                                'less'
-                            );
-    public  $cssCompressor  = new YUICompressor(		// css compresssor
-                                dirname(__FILE__).'/deploy/yuicompressor-2.4.7.jar',
-                                dirname(__FILE__),
-                                array('type' => 'css')
-                            );
-    public 	$jsCompressor   = new YUICompressor(		// js compressor
-                                dirname(__FILE__).'/deploy/yuicompressor-2.4.7.jar',
-                                dirname(__FILE__),
-                                array('type' => 'js')
-                            );
+    public  
+    public  $cssCompressor  = null;
+    public 	$jsCompressor   = null;
 
     /**
      *  Now time for a deploy - get the POST data
      */
     function __construct($settings){
 
-        foreach ($settings as $key->$val) {
+        foreach ($settings as $key => $val) {
             $this->settings[$key] = $val;
         }
 
+        $this->cssCompressor = new YUICompressor(        // css compresssor
+                                    dirname(__FILE__).'/yuicompressor-2.4.7.jar',
+                                    dirname(__FILE__).'/tmp',
+                                    array('type' => 'css')
+                                );
+        $this->jsCompressor =  new YUICompressor(        // js compressor
+                                    dirname(__FILE__).'/yuicompressor-2.4.7.jar',
+                                    dirname(__FILE__).'/tmp',
+                                    array('type' => 'js')
+                                );
+
         // check that we have rights to deploy - IP check
         if (!in_array($_SERVER['REMOTE_ADDR'], $this->ips)) {
-            GitHubAutoDeployment::log('error', 'Attempt to make a deploy from a not allowed IP: ' . $_SERVER['REMOTE_ADDR'], true);
+            // debug
+            //GitHubAutoDeployment::log('error', 'Attempt to make a deploy from a not allowed IP: ' . $_SERVER['REMOTE_ADDR'], true);
         }
 
         // We received json object - decode it
         $this->data = json_decode(stripslashes($this->settings['payload']));
 
-        // debug
-        GitHubAutoDeployment::log('settings 1', print_r($this->settings,true), false);
-
         // If branch is not specified
         if(!$this->testBranch()) {
             die;
         }
-
-        // debug
-        GitHubAutoDeployment::log('settings 2', print_r($this->settings,true), false);
 
 		$this->deploy();
     }
@@ -84,7 +83,8 @@ class GitHubAutoDeployment {
         if(!isset($this->settings['branches']) || empty($this->settings['branches'])) {
             return false;
         }
-        $this->branch = substr($this->data-ref,strrchr($this->data-ref,"/")+1);
+
+        $this->branch = substr(strrchr($this->data->ref,"/"),1);
 
         // TO-DO: asterix search
         if (!array_key_exists($this->branch, $this->settings['branches'])) {
@@ -158,19 +158,21 @@ class GitHubAutoDeployment {
         $file_info = pathinfo($file);
 
         // check file
-        foreach ($this->ex_files as $ex_file) {
+        foreach ($this->settings['excludeFiles'] as $ex_file) {
             
             // file type
-            if (strpos('*.', $ex_file, 0) === 0) {
+            if (strpos($ex_file,'*.') === 0) {
                 $extension = substr($ex_file,2);
+
                 if ($extension == $file_info['extension']) {
                     return true;
                 }
                         
             // file 
-            } elseif (strpos('*', $ex_file, 0) === 0) {
+            } elseif (strpos($ex_file, '*') === 0) {
                 $filename = substr($ex_file,1);
-                if ($filename == $file_info['filename']) {
+
+                if ($filename == $file_info['basename']) {
                     return true;
                 }
             
@@ -181,7 +183,7 @@ class GitHubAutoDeployment {
         }
         
         // check dir
-        foreach ($this->ex_dirs as $ex_dir) {
+        foreach ($this->settings['excludeDirs'] as $ex_dir) {
             if (strpos($ex_dir, $file_info['dirname'], 0) === 0) {
                 return true;
             }
@@ -195,9 +197,13 @@ class GitHubAutoDeployment {
      */
     static function log($status, $message, $die = false){
         file_put_contents(GitHubAutoDeployment::LOG_FILE,
-                            date('Y.m.d@H:i:s') . ' - ' . strtoupper($status) . ' - ' . $message . "\n",
+                            date('d.m.Y @ H:i:s') . ' - ' . strtoupper($status) . ' - ' . $message . "\n",
                             FILE_APPEND
                         );
+
+        // debug
+        print(date('d.m.Y @ H:i:s') . ' - ' . strtoupper($status) . ' - ' . $message);
+
         if ($die)
             die;
     }
